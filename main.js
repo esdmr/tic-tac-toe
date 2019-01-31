c8ff0cc78f0486b7786f520b034ad31f1274c496
esdmr <esdmr0@gmail.com>
2019-01-31 14:27:31 +0330

    Update main

--- main.js
+++ main.js
@@ -9,14 +9,10 @@ global.char = Object.freeze({
 const range = require("./lib/range");
 const checkWin = require("./lib/check-win");
 const toBoard = require("./lib/to-board");
-
-/**
- * @param  {...number} moves
- * @returns {void}
- */
-function done(...moves) {
-	console.log(moves);
-}
+const fs = require("fs");
+const result = new Set();
+let current = 0;
+console.log(""); // Output New Line
 
 // O(1'814'400)
 for (const I of range(0, 8))
@@ -29,12 +25,16 @@ for (const I of range(0, 8))
 							for (const P of range(0, 8, I, J, K, L, M, N, O))
 								for (const Q of range(0, 8, I, J, K, L, M, N, O, P))
 									for (const TARGET of range(5, 9)) {
+										++current;
+										process.stdout.write(`\rProgress: ${(current / 18144).toFixed(3)}% ${current}/1'814'400, Found: ${result.size}/1814400 ${(result.size / 18144).toFixed(3)}% of whole, ${result.size}/${current} ${(result.size / current).toFixed(3)}% of current.`);
 										const FINAL = [I, J, K, L, M, N, O, P, Q].slice(0, TARGET);
-										const X_WINS = checkWin(global.char.x, ...toBoard(FINAL));
-										const O_WINS = checkWin(global.char.o, ...toBoard(FINAL));
+										const X_WINS = checkWin(global.char.x, ...toBoard(...FINAL));
+										const O_WINS = checkWin(global.char.o, ...toBoard(...FINAL));
 
 										if (X_WINS || O_WINS) {
-											done(...FINAL);
+											result.add(FINAL);
 											break;
 										}
 									}
+
+fs.writeFileSync([...result].map(toBoard).join("\n"));
