a2810daa8506a076dbbc9ac534e48ea8bb0a45ce
esdmr <esdmr0@gmail.com>
2019-02-01 09:46:04 +0330

    Add colors ^_^

--- main.js
+++ main.js
@@ -10,9 +10,10 @@ const range = require("./lib/range");
 const checkWin = require("./lib/check-win");
 const toBoard = require("./lib/to-board");
 const fs = require("fs");
+const colors = require("colors");
 const result = new Set();
 let current = 0;
-console.log(""); // Output New Line
+process.stdout.write("\n"); // Output New Line
 
 // O(1'814'400)
 for (const I of range(0, 8))
@@ -26,15 +27,36 @@ for (const I of range(0, 8))
 								for (const Q of range(0, 8, I, J, K, L, M, N, O, P))
 									for (const TARGET of range(5, 9)) {
 										++current;
-										process.stdout.write(`\rProgress: ${(current / 18144).toFixed(3)}% ${current}/1'814'400, Found: ${result.size}/1814400 ${(result.size / 18144).toFixed(3)}% of whole, ${result.size}/${current} ${(result.size / current).toFixed(3)}% of current.`);
+										
+										process.stdout.write("\rProgress: ".blue.bold
+											+ (current / 18144).toFixed(3).yellow
+											+ "% "
+											+ current.toString().red
+											+ "/"
+											+ `1'814'400`.green
+											+ ", "
+											+ "Found: ".blue.bold
+											+ (result.size / 18144).toFixed(3).yellow
+											+ "% "
+											+ result.size.toString().red
+											+ "/"
+											+ "1'814'400".green
+											+ ", "
+											+ (result.size / current).toFixed(3).yellow
+											+ "% "
+											+ result.size.toString().red
+											+ "/"
+											+ current.toString().green);
 										const FINAL = [I, J, K, L, M, N, O, P, Q].slice(0, TARGET);
 										const X_WINS = checkWin(global.char.x, ...toBoard(...FINAL));
 										const O_WINS = checkWin(global.char.o, ...toBoard(...FINAL));
 
 										if (X_WINS || O_WINS) {
 											result.add(FINAL);
+											current += 9 - TARGET;
 											break;
 										}
 									}
 
+process.stdout.write("\n"); // Output New Line
 fs.writeFileSync([...result].map(toBoard).join("\n"));
